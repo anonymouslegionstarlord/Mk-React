bb28b5b3e4340578c24409e90b72a5f5531e5002
anonymouslegionstarlord <radheyshyamdubey51@gmail.com>
2024-11-13 19:31:33 +0530

    counter

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,18 +1,16 @@
-
-
-import Chai from "./Chai"
-
+import React from 'react';
+import Chai from "./Chai";
 
 function App() {
-  const username = "chai aur code"
+  const username = "chai aur code";
 
   return (
-    <>
-    <Chai/>
-    <h1>chai aur react {username}</h1>
-    <p>test para</p>
-   </>
-  )
+    <React.Fragment>
+      <Chai />
+      <h1>chai aur react {username}</h1>
+      <p>test para</p>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
