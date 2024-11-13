28ef101ed467df8e1416415e69c7d540fbf27ba8
anonymouslegionstarlord <radheyshyamdubey51@gmail.com>
2024-11-14 03:12:54 +0530

    tailwind

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,40 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(15);
 
-  let [counter,setCounter]= useState(15)
-  
-// let counter = 15
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
 
-const addValue =() => {
-  if(value<=20){
-  setCounter(counter+1 )
-  }
-}
-const removeValue =() => {
-  if (value>=0){
-  setCounter(counter-1 )
-  
-  }
-}
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
-return (
+  return (
     <>
       <h1>Chai aur React</h1>
       <h2>Counter Value: {counter}</h2>
 
-      <button 
-      onClick={addValue}
-      >Add Value{counter}</button>
+      <button onClick={addValue}>Add Value</button>
       <br />
-      <button 
-      onClick={removeValue}>Remove Value{counter}</button>
-      <p>footer:{counter}</p>
+      <button onClick={removeValue}>Remove Value</button>
+      <p>Footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
