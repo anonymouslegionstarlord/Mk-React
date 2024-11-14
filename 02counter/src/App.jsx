38c01b97e5639ef041acee180a26707bb950ed76
anonymouslegionstarlord <radheyshyamdubey51@gmail.com>
2024-11-14 14:05:49 +0530

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
 
   const addValue = () => {
     if (counter < 20) {
-      setCounter(counter + 1);
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      setCounter(prevCounter=> prevCounter+1);
+      
     }
   };
 
